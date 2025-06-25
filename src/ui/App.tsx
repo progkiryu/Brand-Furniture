d9e13d536c8056321979b4fb8fc9f3dd085edafd
J <Jdng13@gmail.com>
2025-06-25 10:03:01 +1000

    setup: react-electron-ts

---
+++ src/ui/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>are we cooked?</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          hol up, let him cook
+        </button>
+        <h3> times cooked: {count} </h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
