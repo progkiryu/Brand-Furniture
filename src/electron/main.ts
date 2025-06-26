d674686d522b07045d39ba3a7d6ed0a4d43f031f
progkiryu <denmesa24@outlook.com>
2025-06-26 12:30:49 +1000

    add comments to electron.ts

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -3,12 +3,22 @@ import path from "path";
 import { isDev } from "./util.js";
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({});
+  // create browser window
+  const mainWindow = new BrowserWindow({
+    // hide top menu bar
+    autoHideMenuBar: true,
+    webPreferences: {
+      // security purposes
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
 
   if (isDev()) {
     // Open live server while in dev mode
     mainWindow.loadURL("http://localhost:5555");
   } else {
+    // Open HTML file when app is built
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
   }
 });
