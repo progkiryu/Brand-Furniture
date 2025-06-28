6b8e89b5e8e695471d160c77a78fd6de76c70b41
progkiryu <denmesa24@outlook.com>
2025-06-28 14:33:29 +1000

    make order schema and model

---
+++ src/electron/schema.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    orderName: String,
+    orderDesc: String,
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
