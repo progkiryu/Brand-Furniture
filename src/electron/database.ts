99252277f0a5d9236781cbffc7ab4c394b36c133
progkiryu <denmesa24@outlook.com>
2025-06-28 11:46:13 +1000

    successful connection to mongodb locally

---
+++ src/electron/database.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config({
+    path: "../../.env"
+});
+
+const app = express();
+const PORT = process.env.USER_PORT || 8000;
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/testing";
+
+export function mongoConnect() {
+    mongoose.connect(MONGO_URL).then(() => {
+        console.log("Successful connection!");
+        app.listen(PORT, () => {
+            console.log(`MongoDB server running on port ${PORT}`);
+        })
+    });
+}
