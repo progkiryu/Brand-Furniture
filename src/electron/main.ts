6326c00eace24286f5261f573748657a2e4e63f9
J <Jdng13@gmail.com>
2025-07-01 01:48:55 +1000

    Added mongoDB connection

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -2,6 +2,13 @@ import { app, BrowserWindow } from "electron";
 import path from "path";
 import { isDev } from "./util.js";
 
+// DB Imports
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 app.on("ready", () => {
   const mainWindow = new BrowserWindow({});
 
@@ -11,4 +18,33 @@ app.on("ready", () => {
   } else {
     mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
   }
+});
+
+// DB
+const appDB = express();
+appDB.use(express.json());
+appDB.use(express.urlencoded({ extended: false }));
+appDB.use(cors());
+appDB.use(cookieParser());
+dotenv.config();
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI ? process.env.MONGO_URI : "";
+const PORT = process.env.port || 5051;
+
+// Connect to mongoDB
+try {
+  const conn = await mongoose.connect(MONGO_URI);
+  console.log(`MongoDB connected: ${conn.connection.host}`);
+} catch (err) {
+  console.log("Error connecting to mongoDB: ", err);
+}
+
+// appDB.get("/", (req, res) => {
+//   res.status(200).send("Hello World");
+// });
+
+appDB.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
