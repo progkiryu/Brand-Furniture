19dbc788da2803e969194498de3482026ffcfc5c
J <Jdng13@gmail.com>
2025-07-03 12:35:10 +1000

    Removed test controller and routes

--- src/electron/database/routes/index.ts
+++ src/electron/database/routes/index.ts
@@ -4,15 +4,12 @@ import jobRoute from "./jobRoute.js";
 import taskRoute from "./taskRoute.js";
 import frameRoute from "./frameRoute.js";
 
-import testRoute from "./test.route.js"; // Remove Later
-
 const router = express.Router();
 
 export default (): express.Router => {
   jobRoute(router);
   taskRoute(router);
   frameRoute(router);
-  testRoute(router); // Remove Later
 
   return router;
 };
