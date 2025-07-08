9e205dab1e44db6e525dbab8050dd11f52e46f9c
Beginner24082005 <114759487+Beginner24082005@users.noreply.github.com>
2025-07-08 15:01:29 +1000

    Changed Link to NavLink in Navbar.tsx

--- src/react/components/Navbar.tsx
+++ src/react/components/Navbar.tsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 function Navbar() {
     return (
         <div id="navBar-container">
             <h1>Brand.</h1>
-            <Link to="/Dashboard">Dashboard</Link>
-            <Link to="/Schedule">Schedule</Link>
-            <Link to="/Analytics">Analytics</Link>
-            <Link to="/Help">Help</Link>  
+            <NavLink to="/Dashboard">Dashboard</NavLink>
+            <NavLink to="/Schedule">Schedule</NavLink>
+            <NavLink to="/Analytics">Analytics</NavLink>
+            <NavLink to="/Help">Help</NavLink>  
         </div>
     )
 }
