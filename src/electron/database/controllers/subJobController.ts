ea8c11cee5cf96c663491122ff776ad4d48268f9
J <Jdng13@gmail.com>
2025-07-08 13:03:24 +1000

    Updated insertSubJob, Updated Schemas

--- src/electron/database/controllers/subJobController.ts
+++ src/electron/database/controllers/subJobController.ts
@@ -53,15 +53,55 @@ export const insertSubJob = async (
   res: express.Response
 ) => {
   try {
-    const result = await schemas.SubJob.create(req.body);
-    if (!result) {
-      throw new Error("Could not insert new Sub-job!");
+    // Search for the main job
+    const jobId = req.body.jobId;
+    if (!jobId) {
+      res
+        .status(404)
+        .json({ message: "Subjob Creation: Failed to find main job." });
     }
-    res.status(200).json(result);
-    return;
+
+    // Assign main jobId to subJob
+    const tempSubJob: SubJob = req.body;
+    tempSubJob.jobId = jobId;
+
+    // Create new subjob in database
+    const subJob = await schemas.SubJob.create(tempSubJob);
+    if (!subJob) {
+      res
+        .status(404)
+        .json({ message: "Subjob Creation: Failed to create subjob." });
+    }
+    const subJobId = subJob._id.toString(); // Convert objectid to string
+
+    // Check if subJob id already exists within the main Job then add/remove
+    const updatedJob = await schemas.Job.updateMany({ _id: jobId }, [
+      {
+        $set: {
+          subJobList: {
+            $cond: [
+              { $in: [subJobId, "$subJobList"] },
+              {
+                $filter: {
+                  input: "$subJobList",
+                  cond: { $ne: ["$$this", subJobId] },
+                },
+              },
+              { $concatArrays: ["$subJobList", [subJobId]] },
+            ],
+          },
+        },
+      },
+    ]);
+    if (!updatedJob) {
+      res.status(404).json({
+        message: "Subjob Creation: Failed to add subjob to main job.",
+      });
+    }
+    // Return newly created subjob
+    res.status(200).json(subJob);
   } catch (err) {
     res.status(400).json(err);
-    return;
   }
 };
 
