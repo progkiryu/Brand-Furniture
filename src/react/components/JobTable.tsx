32a056fd00b107220207fbc9df46fb14606e5598
progkiryu <denmesa24@outlook.com>
2025-07-13 18:29:31 +1000

    fix notification api syntax

--- src/react/components/JobTable.tsx
+++ src/react/components/JobTable.tsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from 'react';
-import AddSubJobFormModal from "../components/AddSubJobFormModal";
-import JobTableRow from './JobTableRow';
 
 
 interface JobTableProps {
