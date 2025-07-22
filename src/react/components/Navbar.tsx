3e3d9285d4670baf09ad783e6be544854fac85be
progkiryu <denmesa24@outlook.com>
2025-07-22 14:42:35 +1000

    make logo clickable that opens up brand furniture website

--- src/react/components/Navbar.tsx
+++ src/react/components/Navbar.tsx
@@ -2,10 +2,17 @@ import { NavLink } from "react-router-dom";
 import logo from "../media/Brand+Furniture+Logo_Web.png"; 
 
 function Navbar() {
+
+  const handleLinkClick = () => {
+    if (window.electron) {
+      window.electron.openExternalLink("https://www.brandfurniture.com.au");
+    }
+  }
+
   return (
     <div id="navBar-container">
       {/* <h1>Brand.</h1> */}
-      <NavLink to="/"><div className="logo-container"><img src={logo}></img></div></NavLink>
+      <div className="logo-container" onClick={handleLinkClick}><img src={logo}></img></div>
       <NavLink to="/Dashboard">Dashboard</NavLink>
       <NavLink to="/Schedule">Schedule</NavLink>
       <NavLink to="/Analytics">Analytics</NavLink>
--- src/react/pages/Dashboard.tsx
+++ src/react/pages/Dashboard.tsx
@@ -108,7 +108,7 @@ function Dashboard() {
       console.error("Failed to create job.");
     }
   };
-
+  
   useEffect(() => {
     const fetchData = async () => {
       // setIsLoading(true);
