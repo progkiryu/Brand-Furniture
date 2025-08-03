c81c1333c12b9b3f7535b9bb9a8e526a49d6941b
progkiryu <denmesa24@outlook.com>
2025-08-03 18:21:38 +1000

    remove unnecessary text (pt.2)

--- src/react/pages/Help.tsx
+++ src/react/pages/Help.tsx
@@ -8,50 +8,9 @@ function Help() {
       <Navbar />
       <div id="first-container">
         <div className="help-content">
-          <h2>How to Use Threadline</h2>
+          <h2>How to Use Threadline:</h2>
 
-          <div className="help-video-placeholder">
-            {/* Replace this with a real vid later */}
-            <div className="video-frame">
-              <p style={{ color: "white" }}>Video Placeholder</p>
-            </div>
-          </div>
-
-          <div className="help-notes">
-            <h3>Key Notes</h3>
-
-            <h4>Dashboard</h4>
-            <ul>
-              <li>
-                Lists job components or sub-jobs that are pinned (first
-                priority).
-              </li>
-              <li>
-                Analytics window details ordered and received parts for
-                progress.
-              </li>
-              <li>
-                Notifications pop up to provide reminders on pinned jobs and
-                order due dates.
-              </li>
-            </ul>
-
-            <h4>Schedule</h4>
-            <ul>
-              <li>
-                Provides a detailed list of all the jobs with search and filter
-                options.
-              </li>
-            </ul>
-
-            <h4>Analytics</h4>
-            <ul>
-              <li>
-                Provides graphs for order types, completion, and volume across
-                timeframes.
-              </li>
-            </ul>
-          </div>
+          <p>Insert user manual link</p>
         </div>
       </div>
     </>
